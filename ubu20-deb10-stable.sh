710464fabe7e02e4dd8d00cfb7c173d3e3a1cdcb
Richi <fitrarizki23@gmail.com>
2024-10-01 10:25:53 +0700

    update repo and log

--- ubu20-deb10-stable.sh
+++ ubu20-deb10-stable.sh
@@ -523,7 +523,7 @@ print_success "Password SSH"
 function udp_mini(){
 clear
 print_install "Memasang Service Limit IP & Quota"
-wget -q https://raw.githubusercontent.com/Adidastore11/adi2/main/config/fv-tunnel && chmod +x fv-tunnel && ./fv-tunnel
+wget -q https://raw.githubusercontent.com/Richieid23/cyberboy-script/main/config/fv-tunnel && chmod +x fv-tunnel && ./fv-tunnel
 
 # // Installing UDP Mini
 mkdir -p /usr/local/kyt/
